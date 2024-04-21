6a2ca4e30a2c768f81671e60c8503c247d75cf8a
namjuju <157003336+namjuju@users.noreply.github.com>
2024-04-21 20:57:33 +0900

    .

--- wjbus/js/main.js
+++ wjbus/js/main.js
@@ -51,12 +51,6 @@ $(document).ready(function(){
 
     });
 
-
-    $('.quick .quick_btn ul li').on('mouseenter', function(){
-        $('.quick .quick_btn ul li').removeClass('active')
-        $(this).addClass('active')
-    });
-
     AOS.init({
         offset: 50, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
         duration: 300, // 애니메이션 효과가 작동되는 시간
