12b524c0e9627e1ad966af6cccbfbf405bb81cd9
namjuju <157003336+namjuju@users.noreply.github.com>
2024-04-25 18:22:40 +0900

    .

--- eleven2/js/main.js
+++ eleven2/js/main.js
@@ -53,38 +53,43 @@ $(document).ready(function(){
 
 
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 3000,
-		disableOnInteraction: true,
-	},
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 6000,
+            disableOnInteraction: true,
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.visual .paging', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
-		},
-	},
-	
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
 
-	navigation: {  /* 이전, 다음 버튼 */
-		nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
-		prevEl: '.visual .prev',  
-	},
-});
-$('.visual .ctrl_btn button.stop').on('click', function(){
-    $(this).hide()
-    $('.visual .visual_btn .ctrl_btn button.play').show()
-    visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-})
-$('.visual .ctrl_btn button.play').on('click', function(){
-    $(this).hide()
-    $('.visual .visual_btn .ctrl_btn button.stop').show()
-    visual_swiper.autoplay.start();  /* 재생 기능 */
-})
+        on: {
+            slideChange: function(){
+                $('.visual	.visual_btn .ctrl_btn .circle_wrap').attr('data-slide', 'slide'+this.realIndex)
+            }
+        },        
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .prev',  
+        },
+    });
+    $('.visual .ctrl_btn button.stop').on('click', function(){
+        $(this).hide()
+        $('.visual .visual_btn .ctrl_btn button.play').show()
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+    })
+    $('.visual .ctrl_btn button.play').on('click', function(){
+        $(this).hide()
+        $('.visual .visual_btn .ctrl_btn button.stop').show()
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+    })
  
  })//$(document).ready
