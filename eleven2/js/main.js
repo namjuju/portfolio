42a1514fdf9c7a20cd623ab83ac8d9dbaa4e981e
namjuju <157003336+namjuju@users.noreply.github.com>
2024-05-07 00:18:50 +0900

    .

--- eleven2/js/main.js
+++ eleven2/js/main.js
@@ -141,4 +141,26 @@ $(document).ready(function(){
         },
     });
 
+    const discount_swiper = new Swiper('.discount .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                spaceBetween: 30, 
+            },
+            1024: {  /* 1024px 이상이 되면 적용 */
+                slidesPerView: "8", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+                spaceBetween: 40,
+            },
+        },
+        navigation: {
+            nextEl: '.discount .next',
+            prevEl: '.discount .prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.discount .pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+    });
+
  })//$(document).ready
